ea804b76f884507e596e8ce7e62f76df6557782a
Dioarya <dioaryaraditya@gmail.com>
2026-04-27 16:46:13 +0700

    fix: Fix null/undefined runtime error for DarkModeToggle

--- src/components/ui/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/ui/DarkModeToggle/DarkModeToggle.tsx
@@ -1,7 +1,6 @@
 import style from "./DarkModeToggle.module.css";
-type DarkModeToggleProps = {};
 
-export default function DarkModeToggle({}: DarkModeToggleProps) {
+export default function DarkModeToggle() {
   const { theme, setTheme } = useTheme();
   const switchTheme = (checked: boolean) => setTheme(checked ? "dark" : "light");
   return <Toggle className={style.toggle} checked={theme === "dark"} onChange={switchTheme} />;
