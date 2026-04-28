eaba0f609aca5e7d656b0eecf83f06e8ab9203a8
Dioarya <dioaryaraditya@gmail.com>
2026-04-29 00:07:06 +0700

    style: Use static dot notation instead of dynamic index operator

--- src/components/ui/Spinner/Spinner.tsx
+++ src/components/ui/Spinner/Spinner.tsx
@@ -2,8 +2,8 @@ import style from "./Spinner.module.css";
 
 export default function Spinner() {
   return (
-    <div className={style["container"]}>
-      <div className={style["spinner"]}></div>
+    <div className={style.container}>
+      <div className={style.spinner}></div>
     </div>
   );
 }
